ab4ef029ddce180a2beae26dcf7f51d1018d0101
franconicolini1 <franconicolini111@gmail.com>
2021-12-13 23:10:18 -0300

    matches with backend

--- src/components/UserMatches.tsx
+++ src/components/UserMatches.tsx
@@ -1,17 +1,17 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { Animal, getAnimales, getAnimal } from "../interfaces/animales";
-import NavBar from "./NavBar";
-import "../css/UserMatches.css";
+import React, { Fragment, useEffect, useState } from 'react';
+import { Animal, getAnimales } from '../interfaces/animales';
+import NavBar from './NavBar';
+import axios from 'axios';
+import '../css/UserMatches.css';
 
 const UserMatches = () => {
-  const [userAnimal, setUserAnimal] = useState<getAnimal>({
-    message: {
-      name: "No hay animal",
-      image:
-        "https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg",
-      petType: "",
-      petGender: "",
-    },
+  const [errorMsg, setErrorMsg] = useState('No tiene ningún match aún')
+  const [userAnimal, setUserAnimal] = useState<Animal>({
+    name: 'No hay animal',
+    image:
+      'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg',
+    petType: '',
+    petGender: '',
   });
 
   const [matches, setMatches] = useState<getAnimales>({
@@ -19,62 +19,91 @@ const UserMatches = () => {
   });
 
   useEffect(() => {
-    fetch("http://localhost:6060/animal")
-      .then((res) => res.json())
-      .then(setUserAnimal);
+    try {
+      axios.get('http://localhost:3001/user')
+      .then(res => {
+        if (res.status === 200) {
+          setUserAnimal({
+            name: res.data.firstName,
+            image: res.data.image,
+            petType: res.data.petType,
+            petGender: res.data.petGender
+          });
+        } else {
+          console.log('Error al obtener foto del usuario')
+        }
+      })
+      .then(() => setErrorMsg(''));
+    } catch(e) {
+      setErrorMsg('No se pudo obtener su foto de usuario')
+    }
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:6060/animales")
-      .then((res) => res.json())
-      .then(setMatches);
+    try {
+      axios.get('http://localhost:3001/mutualmatches')
+      .then(res => {
+        if (res.status === 200) {
+          setMatches(res.data.mutualmatches);
+        } else {
+          console.log('Error al obtener matches')
+        }
+      })
+      .then(() => setErrorMsg(''));
+    } catch(e) {
+      setErrorMsg('No se pudo obtener sus matches')
+    }
   }, []);
 
   return (
     <>
       <NavBar></NavBar>
-      <div className="container">
-        <div className="col pet-photo-container mt-4">
+      <div className='container'>
+        <div className='col pet-photo-container mt-4'>
         <img
-            src={userAnimal.message.image}
-            alt="petPhoto"
-            className="pet-photo col-sm-2 shadow mt-4"
+            src={userAnimal.image}
+            alt='petPhoto'
+            className='pet-photo col-sm-2 shadow mt-4'
           />
-          <h1 className="text-center pet_title mt-4 mb-5 overflow-hidden">
-            {userAnimal.message.name}
+          <h1 className='text-center pet_title mt-4 mb-5 overflow-hidden'>
+            {userAnimal.name}
           </h1>
 
         </div>
-        <div className="container-flex p-2">
+        <div className='container-flex p-2'>
           {matches.message.length > 0 
           ?
           matches.message.map((animal: Animal) => {
             return (
-                <div className="card col-sm-3 match_pet_card shadow mb-5">
+                <div className='card col-sm-3 match_pet_card shadow mb-5'>
                   <img
-                    className="match_pet_img"
+                    className='match_pet_img'
                     src={animal.image}
-                    alt="petImage"
+                    alt='petImage'
                   />
-                  <div className="card-body">
-                    <h5 className="card-title overflow-hidden">{animal.name}</h5>
-                    <p className="match_card_text card-text">
+                  <div className='card-body'>
+                    <h5 className='card-title overflow-hidden'>{animal.name}</h5>
+                    <p className='match_card_text card-text'>
                       Es un perro chiquito y bonito.
                     </p>
                   </div>
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">{animal.petType}</li>
-                    <li className="list-group-item">{animal.petGender}</li>
+                  <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'>{animal.petType}</li>
+                    <li className='list-group-item'>{animal.petGender}</li>
                   </ul>
                 </div>
             );
-          })
+            })
           :
-          <div >
-            No hay Matcheos para tu animal
-            </div>
-
+          <div>
+            {
+              errorMsg ? 
+              <p className='text-danger h6'>{ errorMsg }</p>
+              :
+              <span></span>
             }
+          </div>
+          }
         </div>
       </div>
     </>
